1554204c96cf9fcdc73b2e5ac6f43ab131b1d5e4
Travis Yoder <trayoda@gmail.com>
2015-02-02 11:11:15 -0700

    address more hound style issues

--- spec/features/searching_spec.rb
+++ spec/features/searching_spec.rb
@@ -94,13 +94,13 @@ RSpec.describe "Searching for media" do
   def sign_in_via_twitter
     OmniAuth.config.test_mode = true
     OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new(
-      {
-        "provider" => "twitter",
-        "info"     => { "name" => "trevor",
-        "nickname" => "trevor_is_da_coolest" },
-        "uid"      => "123456",
-        "extra"    => { "raw_info" => { "profile_image_url_https" => "http://robohash.org/1.png" } }
-      }
+      "provider" => "twitter",
+      "info"     => { "name" => "trevor",
+      "nickname" => "trevor_is_da_coolest" },
+      "uid"      => "123456",
+      "extra"    => { "raw_info" =>
+                    { "profile_image_url_https" =>
+                      "http://robohash.org/1.png" } }
     )
 
     visit root_path
