663e759a7b35a4fdaff3e8488eaabb215d982827
Travis Yoder <trayoda@gmail.com>
2015-03-03 09:20:11 -0700

    setup some memoization

--- app/services/omdb_service.rb
+++ app/services/omdb_service.rb
@@ -2,7 +2,7 @@ class OmdbService
   attr_reader :connection
 
   def initialize
-    @connection = Faraday.new(url: "http://omdbapi.com/")
+    @connection ||= Faraday.new(url: "http://omdbapi.com/")
   end
 
   def media(title, media_type, year)
