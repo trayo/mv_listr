3d897c77d7c0915c9518407b50cd4b3667355081
Travis Yoder <trayoda@gmail.com>
2015-03-24 17:13:43 -0600

    removed the watched recommendations resource and replaced it with a get to the recommendations controller with a watched status hash
    same for unwatched recommendations

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :sessions, only: [:create]
-  resources :recommendations, only: [:index,  :create, :update, :destroy]
+  resources :recommendations, only: [:create, :update, :destroy]
 
-  resources :watched_recommendations, only: [:destroy]
+  get '/recommendations', to: 'recommendations#index', watched_status: false
+  get '/recommendations/watched', to: 'recommendations#index', watched_status: true
 
   resources :twilio, only: [:create]
 
-  get '/recommendations/watched', to: 'watched_recommendations#index'
   delete '/logout', to: 'sessions#destroy'
 
   get '/auth/twitter'
