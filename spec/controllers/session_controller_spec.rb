941b45f0327a67c193a5b5be339293c9fb098a64
Travis Yoder <trayoda@gmail.com>
2015-04-01 14:56:35 -0600

    pulled auth_data out into a method to reduce duplication
    wrote a test for the development user path

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -2,23 +2,9 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, :type => :controller do
   describe 'omniauth' do
-    let(:auth_data) do
-      { 'provider' => 'twitter',
-        'info' => {'name' => 'Alice Smith',
-                   'nickname' => 'asmith'},
-        'uid' => '123456',
-        'extra' => {'raw_info' => {'profile_image_url_https' => 'http://robohash.org/1.png'}}
-      }
-    end
 
     it "logs in a new user" do
-      @request.env["omniauth.auth"] = {
-        'provider' => 'twitter',
-        'info' => {'name' => 'Alice Smith',
-                   'nickname' => 'asmith'},
-        'uid' => '123456',
-        'extra' => {'raw_info' => {'profile_image_url_https' => 'http://robohash.org/1.png'}}
-      }
+      @request.env["omniauth.auth"] = auth_data
 
       post :create
       user = User.find_by(uid: "123456")
@@ -27,19 +13,41 @@ RSpec.describe SessionsController, :type => :controller do
     end
 
     it "logs in an existing user" do
-      @request.env["omniauth.auth"] = {
-        'provider' => 'twitter',
-        'info' => {'name' => 'Bob Jones',
-                   'nickname' => 'bjones'},
-        'uid' => '654321',
-        'extra' => {'raw_info' => {'profile_image_url_https' => 'http://robohash.org/1.png'}}
-      }
-      user = create(:user, uid: "654321")
+      @request.env["omniauth.auth"] = auth_data
+
+      user = create(:user, uid: "123456")
 
       post :create
 
       expect(User.count).to eq(1)
-      expect(User.first.uid).to eq("654321")
+      expect(User.first.uid).to eq("123456")
     end
+  end
+
+  describe "development user" do
+    it "logs in a user" do
+      user = create(:user, name: "Bart Simpson", uid: "1")
+
+      post :create, "uid" => user.uid
+
+      expect(session["user_id"]).to eq User.last.id
+    end
+  end
+
+  def auth_data
+    {
+      'provider' => 'twitter',
+      'info' =>
+        {
+          'name' => 'Alice Smith',
+          'nickname' => 'asmith'
+        },
+      'uid' => '123456',
+      'extra' =>
+        {
+          'raw_info' =>
+            { 'profile_image_url_https' => 'http://robohash.org/1.png' }
+        }
+    }
   end
 end
